583130d03ffaf0a17544ad461faa7da198e0f796
DEEPCORE-SYSTEM <eslicardenasvivanco@gmail.com>
2026-04-25 10:39:37 -0500

    Simplify before_install.sh script

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-if [ -d /home/ubuntu/app/ ]; then
-    sudo rm -rf /home/ubuntu/app/*
-fi
-sudo mkdir -p /home/ubuntu/app/
-sudo chown -R ubuntu:ubuntu /home/ubuntu/app/
+sudo rm -rf /home/ubuntu/app
+sudo mkdir -p /home/ubuntu/app
+sudo chown -R ubuntu:ubuntu /home/ubuntu/app
+
 echo "[BeforeInstall] Directorio limpio y preparado"
